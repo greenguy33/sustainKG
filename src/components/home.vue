4744dafb8bfbafda0868415acb718a8ed2fa4a17
songqingyu <1297889314@qq.com>
2021-11-01 00:49:35 -0700

    auto saved

--- src/components/home.vue
+++ src/components/home.vue
@@ -978,7 +978,7 @@
                         }
 
 
-                        navigator.sendBeacon("https://graphdb.ics.uci.edu/postUserGraph", JSON.stringify(data));
+                        navigator.sendBeacon("http://172.18.0.3:8080/postUserGraph", JSON.stringify(data));
 
                     // navigator.sendBeacon("http://graphdb.ics.uci.edu:8080/api/postUserGraph", JSON.stringify(data));
                 }
