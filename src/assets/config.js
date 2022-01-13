c6c0066d64bb145ac659e69928a8f5c7d7130d92
songqingyu <1297889314@qq.com>
2022-01-13 15:09:01 -0800

    config file changed

--- src/assets/config.js
+++ src/assets/config.js
@@ -5,13 +5,13 @@ let config = {
     API_URL : 'http//localhost:8089',
     admin_users : ['hfreedma', 'qingys1', 'wmt'],
     // relationships : ["causes", "inhibits", "produces", "has part", "part of"],
-    relationships : ["causes"],
+    relationships : ["causes","inhibits"],
         //relationships = 'relations.txt',
     concepts : 'Wikipedia',
     //concepts = {"Motion", "Momentum", "Impulse", "Torque", "Power"}
     //concepts = concepts.txt
     Voting : true,
-    Citations : false,
+    Citations : true,
     Collaborative : true
 
 };
