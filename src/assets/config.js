57eea0288452c2ecec708eb4630df274d2f59c84
Hayden Freedman <hayden.freedman@gmail.com>
2022-01-18 16:43:52 -0500

    Update config.js

--- src/assets/config.js
+++ src/assets/config.js
@@ -5,13 +5,13 @@ let config = {
     API_URL : 'http//localhost:8089',
     admin_users : ['hfreedma', 'qingys1', 'wmt'],
     // relationships : ["causes", "inhibits", "produces", "has part", "part of"],
-    relationships : ["causes","inhibits"],
+    relationships : ["causes"],
         //relationships = 'relations.txt',
     concepts : 'Wikipedia',
     //concepts = {"Motion", "Momentum", "Impulse", "Torque", "Power"}
     //concepts = concepts.txt
     Voting : true,
-    Citations : true,
+    Citations : false,
     Collaborative : true
 
 };
