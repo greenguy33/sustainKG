209ef44d0af5c6f4d852a3b09b805fed0c6b56d4
songqingyu <1297889314@qq.com>
2022-01-28 22:21:14 -0800

    layout position

--- src/components/home.vue
+++ src/components/home.vue
@@ -2816,7 +2816,7 @@
                     .attr("preserveAspectRatio", "xMidYMid meet")//自适应容器大小
                     // .attr('width', 921)
                     // .attr('height', 899)
-                    // .attr("viewBox", "-500 -200 2000 2000")
+                    .attr("viewBox", "-500 -200 3000 1500")
                     // .attr("viewBox", "0 0 1000 1000")
                     .call(zoom)
 
