a1b5570993c8009050de5e628db259765c8a47b3
songqingyu <1297889314@qq.com>
2022-03-11 23:09:41 -0800

    test drag add links (show the start node and end node)

--- src/components/home.vue
+++ src/components/home.vue
@@ -3744,7 +3744,7 @@
                         d3.selectAll('.d3-context-menu').html('');
                         let list = d3.selectAll('.d3-context-menu').append('ul');
                         console.log('right-select',data,select)
-                        list.append('li')
+                        list.select('ul').append('li')
                             .html(select.source.properties.name + ' ' +
                                 select.label + ' ' + select.target.properties.name )
                         list.selectAll('li').data(menu).enter()
