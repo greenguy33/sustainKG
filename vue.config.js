5007ef5c227c7e6e15dd1caf38bd368d6ec816e7
songqingyu <1297889314@qq.com>
2022-04-06 12:56:45 -0700

    shibboleth false

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: './',
+    publicPath: '/',
     runtimeCompiler: true, //关键点在这
     devServer: {
         proxy: {
@@ -7,10 +7,10 @@ module.exports = {
             '/api': {
                 // target: 'http://localhost:8089/', //这里填写项目真实的后台接口地址
                 // target: 'http://172.18.0.3:8080/', //这里填写项目真实的后台接口地址
-                target: 'http://localhost:8080/', //这里填写项目真实的后台接口地址
+                target: 'http://127.0.0.1:8080/', //这里填写项目真实的后台接口地址
                 changOrigin: true, //设置允许跨域
                 pathRewrite: {//这个重写不可省略！因为我们真正请求的地址并不含 /api
-                    '/api': ''
+                    '^/api': ''
                     /* 当我们在浏览器中看到请求的地址为：http://localhost:8080/api/data/getdata 时
                     因为重写了 /api ，所以实际上访问的地址是：http://x.x.x.x:x/data/getdata，
                     */
