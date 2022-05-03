e9571f585419d956d860c525f1af2fa2c1c7cf57
Hayden Freedman <hayden.freedman@gmail.com>
2022-05-03 12:33:25 -0600

    Update config.js

--- src/assets/config.js
+++ src/assets/config.js
@@ -2,18 +2,18 @@
 
 let config = {
 
-    API_URL : 'http//localhost:8089',
     admin_users : ['hfreedma', 'qingys1', 'wmt'],
-    relationships : ["causes", "reduces", "controls", "has part", "part of", "opposes", "contributing factor of", "instance of", "described by source", "replaces", "location", "opposite of", "subclass of", "objective of property of action", "has quality", "facet of"],
+    relationships : ["causes", "reduces", "controls", "has part", "part of", "opposes", "contributing factor of", "instance of", "described by source", "replaces", "location", "opposite of", "subclass of", "objective of property of action", "has quality", "facet of", "main subject"],
     // relationships : ["causes"],
     //relationships =: 'relations.txt',
-    //concepts : 'Wikipedia',
-    concepts : ["Motion", "Momentum", "Impulse", "Torque", "Power"],
+    concepts : 'Wikipedia',
+    // concepts : ["Motion", "Momentum", "Impulse", "Torque", "Power","Help Out"],
+    // concepts : [],
     //concepts : concepts.txt,
     Voting : true,
     Citations : false,
     Collaborative : true,
-    useShibboleth : false
+    useShibboleth : true
 
 };
 
