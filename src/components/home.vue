5f6019d1d33d1e49bb9ed77c8b210d3254eafa89
songqingyu <1297889314@qq.com>
2022-05-03 16:22:17 -0700

    websocket

--- src/components/home.vue
+++ src/components/home.vue
@@ -992,17 +992,13 @@
             initWebSocket(){
                 try {
 
-                let protocol = location.protocol === 'https'
-                    ? 'wss://172.18.0.3:8080/connectToWebsocket'
-                    : 'ws://172.18.0.3:8080/connectToWebsocket';
-                console.log(protocol)
-                // let protocol = 'wss://172.18.0.3:8080/connectToWebsocket'
-
                 // let protocol = location.protocol === 'https'
                 //     ? 'wss://172.18.0.3:8080/connectToWebsocket'
                 //     : 'ws://172.18.0.3:8080/connectToWebsocket';
                 // console.log(protocol)
-                // let protocol = 'wss://172.18.0.3:8080/connectToWebsocket'
+                let protocol = 'wss://172.18.0.3:8080/connectToWebsocket'
+
+
 
 
                 // this.websocket = new WebSocket(protocol,'echo-protocol');
